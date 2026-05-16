94d608ebf062bd9a93c1cfada22b2355298dc9c9
fourgeaudpl-lang <fourgeaudpl@gmail.com>
2026-05-16 11:21:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
 const CCYS = ['USD','EUR','GBP','JPY','CAD','AUD','NZD','CHF'];
 
 const CENTRAL_BANKS = [
-    { code: 'FED',  ccy: 'USD', fred: 'IRSTCB01USM156N' },
-    { code: 'ECB',  ccy: 'EUR', fred: 'IRSTCB01EZM156N' },
-    { code: 'BOE',  ccy: 'GBP', fred: 'IRSTCB01GBM156N' },
-    { code: 'BOJ',  ccy: 'JPY', fred: 'IRSTCB01JPM156N' },
-    { code: 'BOC',  ccy: 'CAD', fred: 'IRSTCB01CAM156N' },
-    { code: 'RBA',  ccy: 'AUD', fred: 'IRSTCB01AUM156N' },
-    { code: 'RBNZ', ccy: 'NZD', fred: 'IRSTCB01NZM156N' },
-    { code: 'SNB',  ccy: 'CHF', fred: 'IRSTCB01CHM156N' }
+    { code: 'FED',  ccy: 'USD', fred: 'FEDFUNDS'        },
+    { code: 'ECB',  ccy: 'EUR', fred: 'ECBDFR'          },
+    { code: 'BOE',  ccy: 'GBP', fred: 'BOERUKM'         },
+    { code: 'BOJ',  ccy: 'JPY', fred: 'IRSTCI01JPM156N' },
+    { code: 'BOC',  ccy: 'CAD', fred: 'INTGSBCAM193N'   },
+    { code: 'RBA',  ccy: 'AUD', fred: 'INTGSBAUM193N'   },
+    { code: 'RBNZ', ccy: 'NZD', fred: 'IR3TIB01NZM156N' },
+    { code: 'SNB',  ccy: 'CHF', fred: 'IR3TIB01CHM156N' }
 ];
 
 const CCY_TO_BANK = {
